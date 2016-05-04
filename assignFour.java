8cd1fede1585a29f13bd100c45c793ec6253423c
kkellyin90 <keith.kelly.a@gmail.com>
2016-05-04 12:29:09 +0530

    Added files via upload

---
+++ assignFour.java
@@ -0,0 +1,27 @@
+
+public class assignFour {
+
+	public static void main(String[] args) {
+		
+		int x = 5;
+		int y = 10;
+		
+		//x + y * 2
+		int a = x + y * 2;
+		System.out.println("The expected value of a is 25, calculated value is "+a);
+		
+		//x - y + 2
+		int b = x - y + 2;
+		System.out.println("The expected value of b is -3, calculated value is "+b);
+		
+		//(x+y) * 2
+		int c = (x+y) * 2;
+		System.out.println("The expected value of c is 30, calculated value is "+c);
+		
+		// y % x
+		int d = y % x;
+		System.out.println("The expected value of d is 0, calculated value is "+d);
+
+	}
+
+}
